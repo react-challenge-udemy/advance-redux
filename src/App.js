91c0809b1faf3d96076f4066972301f2d5503775
parisa <mbkparisa@gmail.com>
2022-04-19 15:20:52 +0430

    create action

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import { Fragment } from "react";
-import { useSelector } from "react-redux";
-import Cart from "./components/Cart/Cart";
-import MainHeader from "./components/Layout/MainHeader";
-import Products from "./components/Shop/Products";
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
+import Cart from './components/Cart/Cart';
+import MainHeader from './components/Layout/MainHeader';
+import Products from './components/Shop/Products';
+import Notification from './components/Ui/Notification';
+import { fetchCartData, sendCartData } from './store/cart-actions';
+
+let isInitial = true;
 
 function App() {
-  const showCart = useSelector((state)=>state.ui.cartIsVisible)
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.change) {
+      dispatch(sendCartData(cart));
+    }
+
+
+  }, [cart, dispatch]);
+
   return (
     <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
       <MainHeader />
       {showCart && <Cart />}
       <Products />
+
     </Fragment>
   );
 }
